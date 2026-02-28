91622e15a26bcec3af685b44c01c419b43f0d966
hesonthemove-ux <hesonthemove@gmail.com>
2026-02-28 18:40:58 +0000

    Add Compliance page with regulatory details

---
+++ app/compliance/page.js
@@ -0,0 +1,11 @@
+export default function Compliance() {
+  return (
+    <main>
+      <h1>Regulatory & Technical Compliance</h1>
+      <p>
+        Details regarding regulatory alignment, licensing context,
+        and technical standards will be outlined here.
+      </p>
+    </main>
+  )
+}
