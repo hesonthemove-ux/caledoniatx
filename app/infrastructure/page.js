211563e8aa3b5d1e2b004645f48930b1c4b53bec
hesonthemove-ux <hesonthemove@gmail.com>
2026-02-28 17:49:38 +0000

    Add Infrastructure component for transmission details

---
+++ app/infrastructure/page.js
@@ -0,0 +1,11 @@
+export default function Infrastructure() {
+  return (
+    <main>
+      <h1>Transmission Infrastructure</h1>
+      <p>
+        Information regarding transmission assets, engineering oversight,
+        and operational resilience will be published here.
+      </p>
+    </main>
+  )
+}
