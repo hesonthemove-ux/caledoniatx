4483ac4497dca3c3b043463f6c9fdec0a557a8e4
hesonthemove-ux <hesonthemove@gmail.com>
2026-02-28 17:47:13 +0000

    Add Navbar component with navigation links

---
+++ app/components/Navbar.js
@@ -0,0 +1,7 @@
+<div style={{ display: "flex", gap: "20px" }}>
+  <a href="/">Home</a>
+  <a href="/carriage">Carriage</a>
+  <a href="/infrastructure">Infrastructure</a>
+  <a href="/compliance">Compliance</a>
+  <a href="/contact">Contact</a>
+</div>
